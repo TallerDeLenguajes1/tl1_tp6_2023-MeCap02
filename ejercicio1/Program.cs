60de8f5162d723294da91fc8a04bca24c8ec30ac
MeCap02 <128094444+MeCap02@users.noreply.github.com>
2023-05-22 19:09:34 -0300

    Add files via upload

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ejercicio 1
+int a;
+Console.WriteLine("Ingrese un valor");
+bool control=int.TryParse(Console.ReadLine(),out a);
+while(control==false){
+    Console.WriteLine("Ingrese un valor correcto");
+    control=int.TryParse(Console.ReadLine(),out a);
+}
+if(a>0){
+    a=-a;
+}
+Console.WriteLine(a);
+Console.WriteLine(control);
---
+++ practico6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Introduccion
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("Valor de a:"+a);
+Console.WriteLine("Valor de b:"+b);
